0a1e6996bee5dc4444f2fb1c32f1fa261f12d04c
Clayton <cmpetersen@hotmail.co.za>
2019-09-21 11:47:36 +0200

    Initial commit

---
+++ src/GenericStack.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class GenericStack <T> {
+
+    private ArrayList<T> items = new ArrayList();
+    private int top;
+
+    public GenericStack() {
+
+        top = 0;
+
+    }
+
+    public static void push(){
+
+
+
+    }
+
+    public static void pop(){
+
+        
+
+    }
+
+
+}
